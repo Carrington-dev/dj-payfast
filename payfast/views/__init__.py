61de609878341f09d599c471c318b42cbb7eaeb0
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2026-01-19 09:56:54 +0200

    Update __init__.py

--- payfast/views/__init__.py
+++ payfast/views/__init__.py
@@ -1,10 +1,10 @@
 from .normal_payment_views import (
-    "checkout_view",
-    "payfast_payment_view",
-    "payment_success_view",
-    "payment_cancel_view",
-    "PayFastNotifyView",
-    "PayFastPaymentModelViewSet",
+    checkout_view,
+    payfast_payment_view,
+    payment_success_view,
+    payment_cancel_view,
+    PayFastNotifyView,
+    PayFastPaymentModelViewSet,
 
 )
 # from .recurring_payment_views import *
